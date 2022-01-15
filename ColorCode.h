6d3f0bd95afb8d0de02ce0c5df19b50c51222196
jyotiaralikatti <48976964+jyotiaralikatti@users.noreply.github.com>
2022-01-15 20:24:52 +0530

    Update ColorCode.h

--- ColorCode.h
+++ ColorCode.h
@@ -1,28 +1,10 @@
-enum MajorColor {WHITE, RED, BLACK, YELLOW, VIOLET};
-enum MinorColor {BLUE, ORANGE, GREEN, BROWN, SLATE};
+#ifndef __COLOR_CODE_H__
+#define __COLOR_CODE_H__
 
-const char* MajorColorNames[] = {"White", "Red", "Black", "Yellow", "Violet"};
-const char* MinorColorNames[] = {"Blue", "Orange", "Green", "Brown", "Slate"};
-
-
-int numberOfMajorColors = sizeof(MajorColorNames) / sizeof(MajorColorNames[0]);
-int numberOfMinorColors = sizeof(MinorColorNames) / sizeof(MinorColorNames[0]);
-
-const int MAX_COLORPAIR_NAME_CHARS = 16;
-
-typedef struct {
-    enum MajorColor majorColor;
-    enum MinorColor minorColor;
-} ColorPair;
-
-/*Functions */
+#include "VariableTypes.h"
+/*.. Function Declations ..*/
 void      ColorPairToString(const ColorPair* colorPair, char* buffer);
 ColorPair GetColorFromPairNumber(int pairNumber);
 int       GetPairNumberFromColor(const ColorPair* colorPair);
 
-void testNumberToPair(int ,
-                    enum MajorColor ,
-                    enum MinorColor );
-void testPairToNumber( enum MajorColor major,
-                    enum MinorColor minor,
-                    int expectedPairNumber);
+#endif
