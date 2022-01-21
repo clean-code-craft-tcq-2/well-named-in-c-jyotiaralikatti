a8a09697ce6daf5e81512b38f1445946ac99e034
jyotiaralikatti <48976964+jyotiaralikatti@users.noreply.github.com>
2022-01-21 20:18:15 +0530

    Update VariableTypes.h

--- VariableTypes.h
+++ VariableTypes.h
@@ -1,8 +1,12 @@
 #ifndef __VARIABLE_TYPES_H__
 #define __VARIABLE_TYPES_H__
 
+#define MAX_COLORPAIR_NAME_CHARS  16
+#define MAX_PAIR_NUMBER 25
 enum MajorColor {WHITE, RED, BLACK, YELLOW, VIOLET};
 enum MinorColor {BLUE, ORANGE, GREEN, BROWN, SLATE};
+
+char colorPairNames[MAX_COLORPAIR_NAME_CHARS];
 
 typedef struct {
     enum MajorColor majorColor;
@@ -10,6 +14,6 @@ typedef struct {
 } ColorPair;
 
 extern int numberOfMinorColors,numberOfMajorColors;
+void Print_reference_manual(void);
 
-#define MAX_COLORPAIR_NAME_CHARS  16
 #endif
